bf0a44da08ff564c6f4dcb68a8bd126081e7aa2b
Yaisa C. Ramirez <81448945+yaisa5ramirez@users.noreply.github.com>
2023-04-20 12:09:11 -0500

    Version usada el miercoles en la prueba

--- motor_paso_paso/motor_paso_paso.ino
+++ motor_paso_paso/motor_paso_paso.ino
@@ -1,9 +1,12 @@
-#define Dir 10     // pin DIR de A4988 a pin 5
-#define Step  11     // pin STEP de A4988 a pin 4
-#define Enable 12
-int encoder_pin = 2;
+#include <Wire.h>
+#define Dir 13     // pin DIR de A4988 a pin 5
+#define Step  12     // pin STEP de A4988 a pin 4
+#define Enable 14
+#define encoder_pin 2
 int iteraciones = 0;
-unsigned int rpm;
+int iteraciones2 = 0;
+int rpm_deseado = 100;
+int rpm_prom;
 unsigned long time_old;
 unsigned int pulses_per_turn = 20; // Depends on the number of spokes on the encoder wheel
 bool flag = false;
@@ -13,82 +16,101 @@ int Pulsos = 0;
 int count = 0;
 int PulsosRotacion = 20;
 int del = 0;
-int new_del = 0;
+int new_del = 1400;
 unsigned long InitialTime = 0;
 unsigned long FinalTime;
 float RPMs;
-/////// PID CONTROL RPMS
-//PID myPID(&RPMs, &new_del, &rpm_deseado, kp, ki, kd, DIRECT);
-float rpm_deseado = 90;
-void setup() {
-  rpm = 0;
-  time_old = 0;
-
-  Serial.begin(9600);
-  pinMode(Step, OUTPUT);  // pin 4 como salida
-  pinMode(Dir, OUTPUT);   // pin 5 como salida
-  pinMode(Enable, OUTPUT);
-  digitalWrite(Enable, LOW);
-  pinMode(encoder_pin, INPUT); // Setting encoder-1 pin as Input
-  // attachInterrupt(digitalPinToInterrupt(encoder_pin), Encoder_fun, RISING);
-  //myPID.SetOutputLimits(300, 20000); // Establecer límites de salida del control PID
-  //myPID.SetMode(AUTOMATIC); // Configurar el modo de control PID como automático
-}
-
-void loop() {
-  if (Serial.available() > 0) { // Verificar si hay datos disponibles en el buffer serial
-    rpm_deseado = Serial.parseInt(); // Leer el entero recibido
-    if (rpm_deseado >= 100) {
-      new_del = int((-0.836 * log(rpm_deseado) + 5.2281) * 1000);
-    }
-    else {
-      new_del = int((-4.367 * log(rpm_deseado) + 20.528) * 1000);
-    }
-    //Serial.println(new_del);
-  }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void mover(int new_del){
+  
+  digitalWrite(Dir, 1);
   digitalWrite(Step, 1);
   delayMicroseconds(new_del);
   digitalWrite(Step, 0);
   delayMicroseconds(new_del); //delayMicroseconds
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void medir_rpm(){
   Encoder = digitalRead(encoder_pin);
-  //Serial.println(new_del);
+  if (iteraciones2 == 0)
+  {
+    rpm_prom = 0;
+  }
   if (Encoder == LOW && Estado == 1)
   {
     Estado = 0;
   }
-
   if (Encoder == HIGH && Estado == 0)
   {
     Pulsos++;
     Estado = 1;
   }
-
   if (Pulsos == PulsosRotacion)
   {
     FinalTime = millis();
     RPMs = 60000 / (FinalTime - InitialTime);
+    rpm_prom = rpm_prom + RPMs; 
     Pulsos = 0;
     InitialTime = FinalTime;
-    //Serial.print("RPM = ");
-    Serial.println(RPMs);
+    iteraciones2++;
+    if (iteraciones2 == 4) {
+      Serial.print("RPM = ");
+      Serial.println(rpm_prom/4);
+      iteraciones2= 0;
+      rpm_prom=0;
+      } 
   }
-  if (((rpm_deseado - 1 <= RPMs) || (rpm_deseado + 1 >= RPMs) ) && (iteraciones == 7)) {
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void corregir(int rpm_deseado){
+    if (((rpm_deseado - 1 <= RPMs) || (rpm_deseado + 1 >= RPMs) ) && (iteraciones == 5)) {
     if (rpm_deseado == RPMs){
       new_del = new_del;
       flag = true;
       }   
     else if ((rpm_deseado > RPMs)&& (flag == false)) {
-
-      new_del --;
+      new_del = new_del-1;
     }
     else if ((rpm_deseado < RPMs) && (flag == false)) {
-      //Serial.println("Menor");
-      new_del ++;
+      new_del = new_del+1;
     }
-
     iteraciones = 0;
   }
   iteraciones ++;
-  //Serial.println(rpm_deseado);
+  }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void receiveEvent(int numBytes) {
+  int value = 0; // Variable to hold incoming integer value
+
+  while (Wire.available() > 0) {
+    Wire.readBytes((uint8_t*)&rpm_deseado, sizeof(rpm_deseado)); // Read incoming integer value
+    //new_del = 800; 
+    //Serial.println(rpm_deseado);
+  }
+  //delay(500);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Wire.begin(0x23);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);
+  Serial.println ("Inicia");
+  pinMode(Step, OUTPUT);  // pin 4 como salida
+  pinMode(Dir, OUTPUT); 
+  pinMode(encoder_pin, INPUT); // pin 5 como salida
+  pinMode(Enable, OUTPUT);
+  digitalWrite(Enable, LOW);  
+  }
+
+
+ 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+mover(new_del);
+medir_rpm(); 
+corregir(rpm_deseado); 
 }
